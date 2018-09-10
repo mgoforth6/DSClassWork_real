675f617dae12d3f79513614e8aedafe74372aef4
mgoforth6 <mgoforth@iu.edu>
2018-09-10 10:42:24 -0400

    get fetch working 9/10

--- js/dashboard.app.js
+++ js/dashboard.app.js
@@ -12,55 +12,16 @@ var dashboardApp = new Vue({
   tasks: [
   {
     "id": 1,
-    "title": "Build UI",
-    "type" : "Story",
-    "size" : "M",
-    "team" : "Noble Carrots",
-    "status": "Started",
-    "start_date": "2018-08-02",
-    "close_date": null,
-    "hours_worked":90,
-    "perc_complete": 95,
-    "current_sprint" : true
-  },
-  {
-    "id": 2,
-    "title": "Update unit tests",
-    "type" : "Story",
-    "size" : "L",
-    "team" : "MS Why Us",
-    "status": "Closed",
-    "start_date": "2018-07-15",
-    "close_date": "2018-08-1",
-    "hours_worked": 40,
-    "perc_complete": 100,
-    "current_sprint" : true
-  },
-  {
-    "id": 3,
-    "title": "Write middleware",
-    "type" : "Epic",
-    "size" : "XL",
-    "team" : "California Dream",
-    "status": "Open",
-    "start_date": null,
-    "close_date": null,
-    "hours_worked": 0,
-    "perc_complete": 0,
-    "current_sprint" : true
-  },
-  {
-    "id": 4,
-    "title": "Completion % not saving",
-    "type" : "Bug",
-    "size" : "XS",
-    "team" : "Luke's Parents",
-    "status": "Open",
-    "start_date": "2018-07-29",
-    "close_date": "",
-    "hours_worked": 27,
-    "perc_complete": 80,
-    "current_sprint" : false
+    "title": '',
+    "type" : '',
+    "size" : '',
+    "team" : '',
+    "status": '',
+    "start_date": '',
+    "close_date": '',
+    "hours_worked": '',
+    "perc_complete": '',
+    "current_sprint" : ''
   }
 ]
 },
@@ -75,6 +36,20 @@ var dashboardApp = new Vue({
       return d;
     },
     log (msg) {
+      alert(msg);
+    },
+    fetchTasks() {
+      fetch( 'https://raw.githubusercontent.com/tag/iu-msis/dev/public/p1-tasks.json' )
+      .then( response => response.json() )
+      //.then(scb, ecb)
+      .then( json => {this.tasks = json} )
+      .catch( function(err) {
+        console.log('FETCH ERROR:')
+        console.log(err)
+      })
     }
+  },
+  created: function() {
+    this.fetchTasks()
   }
 })
